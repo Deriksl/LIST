512d3935e74b549e2c550a420153b887e682681c
Deriksl <133131434+Deriksl@users.noreply.github.com>
2023-07-08 00:42:25 -0600

    Add files via upload

--- Tarea List/List.h
+++ Tarea List/List.h
@@ -1,9 +1,3 @@
-#include <vector>
-
-int main() {
-    std::vector<int> myVector = { 1, 1, 2, 3, 4, 2, 3, 1 };
-}
-
 #pragma once
 #ifndef LIST_H
 #define LIST_H
@@ -19,7 +13,7 @@ private:
 
 public:
     List() {} // Constructor por defecto
-    List(const std::initializer_list<T>& elements); // Constructor con parámetros
+    List(T* in_data, int in_size); // Constructor con parámetros
     void PushBack(T element); // Agregar un elemento al final de la lista
     void Print(); // Imprimir los elementos de la lista
     void Reverse(); // Invertir el orden de los elementos en la lista
@@ -27,9 +21,10 @@ public:
 };
 
 template <typename T>
-List<T>::List(const std::initializer_list<T>& elements) {
-    for (const auto& element : elements) {
-        data.push_back(element);
+List<T>::List(T* in_data, int in_size) {
+    for (int i = 0; i < in_size; i++) {
+        // Agregar cada elemento del array in_data a la lista data
+        data.push_back(in_data[i]);
     }
 }
 
